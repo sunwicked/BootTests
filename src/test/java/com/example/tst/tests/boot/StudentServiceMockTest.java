7efc582f5857a4e5b2b98fe314e1de8fe2afab10
sunwicked <sunnykraditya@gmail.com>
2023-09-13 14:43:57 +0530

    Mocking Spring Boot

--- src/test/java/com/example/tst/tests/boot/StudentServiceMockTest.java
+++ src/test/java/com/example/tst/tests/boot/StudentServiceMockTest.java
@@ -1,2 +1,35 @@
-package com.example.tst.tests.boot;public class StudentServiceMockTest {
+package com.example.tst.tests.boot;
+
+import com.example.tst.tests.boot.daos.StudentDao;
+import com.example.tst.tests.boot.services.StudentService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StudentServiceMockTest {
+
+    @Test
+    void testfindTotal() {
+
+        StudentDao studentDaoMock = mock(StudentDao.class);
+        when(studentDaoMock.getMarks()).thenReturn(new int[] {15, 20, 5});
+
+        StudentService studentService = new StudentService(studentDaoMock);
+        int total = studentService.findTotal();
+        assertEquals(40, total);
+    }
+
+    @Test
+    void testfindTotal_EmptyArray() {
+        StudentDao studentDaoMock = mock(StudentDao.class);
+        when(studentDaoMock.getMarks()).thenReturn(new int[] {});
+
+        StudentService studentService = new StudentService(studentDaoMock);
+        int total = studentService.findTotal();
+        assertEquals(0, total);
+    }
+
 }
+
